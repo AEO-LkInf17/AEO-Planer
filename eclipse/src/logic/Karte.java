e38c80c1ff4be838aa3a805f5303ee9152acd9d2
eb0720 <eb0720@c042opsi4.aeo.local>
2019-03-15 08:25:37 +0100

    1. Version von Karte

---
+++ eclipse/src/logic/Karte.java
@@ -0,0 +1,19 @@
+package logic;
+
+public class Karte {
+	
+private int zahl;
+
+public Karte (int zahl) {
+	this.zahl= zahl;
+}
+
+public void setZahl (int zahl) {
+	this.zahl = zahl;
+}
+
+public int getZahl () {
+	return zahl;
+}
+ 
+}
