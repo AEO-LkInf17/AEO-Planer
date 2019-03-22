24b2d4c096dc67dbb34e933fb9bc6f9a178c7094
mc0743 <mc0743@c036opsi4.aeo.local>
2019-03-22 08:29:21 +0100

    imports (nötige)

--- eclipse/src/skipbo/network/NetworkTest.java
+++ eclipse/src/skipbo/network/NetworkTest.java
@@ -1,5 +1,18 @@
 package skipbo.network;
 
+/*
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import java.net.ServerSocket;
+*/
+
 public class NetworkTest {
 
 	public static void main(String[] args) {
