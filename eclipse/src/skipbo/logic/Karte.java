7a7285a74ae6468b1cafb80aa052b650444c83f2
eb0720 <eb0720@c042opsi4.aeo.local>
2019-03-29 08:23:36 +0100

    # WARNING: head commit changed in the meantime
    ESt

--- eclipse/src/skipbo/logic/Karte.java
+++ eclipse/src/skipbo/logic/Karte.java
@@ -4,17 +4,34 @@ package skipbo.logic;
 public class Karte {
 	
 	private int zahl;
+	private boolean Skipbo;
+	private int gewuenschterWert;
 
-	public Karte (int zahl) {
+	public Karte (int zahl, boolean Skibo, int gewuenschterWert) {
 		this.zahl= zahl;
+		this.Skipbo= Skipbo;
+		this.gewuenschterWert= gewuenschterWert;
 	}
-
-	public void setZahl (int zahl) {
-		this.zahl = zahl;
-	}
-
 	public int getZahl () {
 		return zahl;
 	}
+	
+	public int getGewuenschterWert() {
+		return this.gewuenschterWert;
+	}
+	
+	public void setGewuenschterWert(int gewuenschterWert) {
+		this.gewuenschterWert = gewuenschterWert;
+	}
  
+	public boolean istSkipbo() {
+		return Skipbo;
+	}
+	
+	public String toString() {
+	if (this.Skipbo) {
+		this.zahl = +1;
+	}
+	return null;
+	}
 }
