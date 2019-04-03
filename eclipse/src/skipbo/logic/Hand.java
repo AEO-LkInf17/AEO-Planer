d9c06d1bbe6426a5479ea8d81daf013649bc49f5
bk0783 <bk0783@c044opsi4.aeo.local>
2019-04-03 12:26:04 +0200

    Erster Entwurf für Klasse Hand. Hat noch keine Funktion

---
+++ eclipse/src/skipbo/logic/Hand.java
@@ -0,0 +1,14 @@
+package skipbo.logic;
+
+public class Hand {
+	//Attribute
+	//private int kartenAnzahl;
+	//private Spieler Spieler;
+	
+	public Hand(int kartenAnzahl, Spieler Spieler) {
+		//this.kartenAnzahl = kartenAnzahl;
+		//this.Spieler = Spieler;
+	}
+	
+
+}
