8e64838ff7e18abef05487eeedbcaf41411ff49c
fc0742 <fc0742@c049opsi4.aeo.local>
2019-04-12 08:29:23 +0200

    ok

--- eclipse/src/skipbo/network/ClientProbe.java
+++ eclipse/src/skipbo/network/ClientProbe.java
@@ -1,5 +1,28 @@
 package skipbo.network;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ClientProbe {
 
+private Socket socket = null;
+private DataInputStream input = null; 
+private DataOutputStream out = null;
+
+public ClientProbe(String address, int port)
+{
+try
+{ 
+socket = new Socket(address, port); 
+System.out.println("Connected"); 
+input = new DataInputStream(System.in);
+out = new DataOutputStream(socket.getOutputStream());
+}
+catch(IOException u)
+{
+
+}
+}
 }
