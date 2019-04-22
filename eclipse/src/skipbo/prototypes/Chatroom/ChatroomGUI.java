7a8fea894711aa150bff752b4628834824e7b55c
Sahan Gökbakar <36935726+Sposace@users.noreply.github.com>
2019-04-22 21:54:42 +0200

    New

--- eclipse/src/skipbo/prototypes/Chatroom/ChatroomGUI.java
+++ eclipse/src/skipbo/prototypes/Chatroom/ChatroomGUI.java
@@ -1,78 +1,76 @@
-package skipbo.prototypes.Chatroom;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
-import javax.swing.AbstractAction;
-import javax.swing.AbstractButton;
-
-import java.awt.event.ActionEvent;
-import javax.swing.Action;
-
-/** Die GUI Klasse des Chatrooms.
- * 
- * @author Merdan
- * 
- * 
- */
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 12.04.2019
+  * @author 
+  */
 
 public class ChatroomGUI extends JFrame {
-
-	private JPanel contentPane;
-	private final Action action = new SwingAction();
-
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ChatroomGUI frame = new ChatroomGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	
-	public ChatroomGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 847, 493);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnSende = new JButton("Sende");
-		btnSende.setAction(action);
-		btnSende.setBounds(688, 308, 133, 135);
-		contentPane.add(btnSende);
-		
-		JTextArea textAreaSchreibfeld = new JTextArea();
-		textAreaSchreibfeld.setBounds(10, 308, 668, 135);
-		contentPane.add(textAreaSchreibfeld);
-		
-		JTextArea textAreaChat = new JTextArea();
-		textAreaChat.setEditable(false);
-		textAreaChat.setBounds(10, 11, 811, 286);
-		contentPane.add(textAreaChat);
-	}
-	
-	/** Mit späterer Referenz zur Client Klasse soll das selber geschriebene direkt in den Chat intigriert werden.
-	 * 
-	 * Problem: Fehler beim Entnehmen des Textes 
-	 */
-	
-		public void actionPerformed(ActionEvent e) {
-
-			String geschriebenes = textAreaSchreibfeld.getText();
-			System.out.println(geschriebenes);
-		}
-	}
-
+  // Anfang Attribute
+  String[] ChatListInhalt = {"one", "two", "three", "four"};
+  
+  private JTextArea jTextArea2 = new JTextArea("");
+  private JScrollPane jTextArea2ScrollPane = new JScrollPane(jTextArea2);
+  private JButton jButton1 = new JButton();
+  
+  private JList<String> jList1 = new JList<String>(ChatListInhalt);
+  
+  private DefaultListModel jList1Model = new DefaultListModel();
+  private JScrollPane jList1ScrollPane = new JScrollPane(jList1);
+    
+    
+  // Ende Attribute
+  
+  public ChatroomGUI(String title) { 
+    // Frame-Initialisierung
+    super(title);
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    int frameWidth = 924; 
+    int frameHeight = 565;
+    setSize(frameWidth, frameHeight);
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setResizable(false);
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    
+    jTextArea2ScrollPane.setBounds(8, 360, 680, 164);
+    cp.add(jTextArea2ScrollPane);
+    jButton1.setBounds(720, 360, 195, 161);
+    jButton1.setText("jButton1");
+    jButton1.setMargin(new Insets(2, 2, 2, 2));
+    jButton1.addActionListener(new ActionListener() { 
+      public void actionPerformed(ActionEvent evt) { 
+        jButton1_ActionPerformed(evt);
+      }
+    });
+    cp.add(jButton1);
+    jList1.setModel(jList1Model);
+    jList1ScrollPane.setBounds(8, 8, 902, 340);
+    cp.add(jList1ScrollPane);
+    // Ende Komponenten
+    
+    setVisible(true);
+  } // end of public ChatroomGUI
+  
+  // Anfang Methoden
+  public void jButton1_ActionPerformed(ActionEvent evt) {
+    DefaultListModel.add(0, "Programmieren");                                //(chatverlaufanzahl, "Programmieren"); 
+  } // end of jButton1_ActionPerformed
+  
+  // Ende Methoden
+  
+  public static void main(String[] args) {
+    new ChatroomGUI("ChatroomGUI");
+  } // end of main
+  
+} // end of class ChatroomGUI
