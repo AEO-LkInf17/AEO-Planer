c419d39676cc3648925f08f474a0f5c9df39009d
edo <edo@192.168.0.15>
2019-04-26 14:52:21 +0200

    Ablagestapel

--- eclipse/src/skipbo/logic/Ablagestapel.java
+++ eclipse/src/skipbo/logic/Ablagestapel.java
@@ -1,5 +1,23 @@
 package skipbo.logic;
 
-public class Ablagestapel {
+public class Ablagestapel extends Stapel {
+   
+	private int maxKarten;
+	private int anzahlStapel;
 
+public Ablagestapel(int maxKarten, int anzahStapel) {
+	this.maxKarten = maxKarten;
+	this.anzahlStapel = anzahlStapel;
+}
+public void setMaxKarten(int nMaxKarten) {
+	this.maxKarten = nMaxKarten;
+}
+
+public void setAnzahlStapel(int nAnzahlStapel) {
+	this.anzahlStapel = nAnzahlStapel;
+}
+public static void main(String[] args) {
+Ablagestapel as = new Ablagestapel(12,4);
+as.setMaxKarten(8);
+}
 }
