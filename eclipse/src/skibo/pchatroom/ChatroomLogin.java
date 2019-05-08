ded0e5bb8676df91325c28ab69f19dac3eb8d054
Sposace <merdanxberg@gmail.com>
2019-05-08 21:36:58 +0200

    Klappt immer noch nicht

---
+++ eclipse/src/skibo/pchatroom/ChatroomLogin.java
@@ -0,0 +1,91 @@
+package skibo.pchatroom;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+/** Login
+ * @author Merdan
+ * Erste Instanz für den Server um IP und Port zu erfragen.
+ */
+
+public class ChatroomLogin extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JTextArea ipArea = new JTextArea();
+	private JTextArea portArea = new JTextArea();
+	
+	static String ip = "";
+	static String port = "";
+
+	/**
+	 * Create the frame.
+	 */
+	public ChatroomLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 167);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		ipArea.setToolTipText("");
+		ipArea.setBounds(10, 27, 207, 22);
+		contentPane.add(ipArea);
+		
+		
+		portArea.setBounds(10, 78, 207, 22);
+		contentPane.add(portArea);
+		
+		JButton connectBtn = new JButton("Verbinde");
+		connectBtn.setBounds(227, 27, 197, 73);
+		contentPane.add(connectBtn);
+		
+		JLabel lblNewLabel = new JLabel("IP");
+		lblNewLabel.setBounds(10, 11, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Port");
+		lblNewLabel_1.setBounds(10, 60, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		connectBtn.addActionListener(this);
+		
+	}
+	/** Action-Methode (Fensterklasse)
+	 * Ip und Port werden den textAreas entnommen 
+	 * und der Inhalt ausgeblendet, sobald der connectionBtn gedrückt wird
+	 *
+	 */
+	public void actionPerformed(ActionEvent e) {
+		ip = ipArea.getText().trim();
+		port = portArea.getText().trim();    //.trim ist eine Methode die es einem erlaubt überflüssige Leerzeichen zu entfernen
+		System.out.println(ip +"\n"+ port);     //"\n" erlaubt es einem eine neue Zeile anzufangen
+		contentPane.setVisible(false);
+	}
+
+	/**
+	 * Main
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChatroomLogin frame = new ChatroomLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
---
+++ eclipse/src/skibo/pchatroom/ChatroomServer.java
@@ -0,0 +1,45 @@
+package skibo.pchatroom;
+
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException; 
+import java.io.InputStream; 
+import java.io.InputStreamReader; 
+import java.io.OutputStream; 
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket; 
+import java.net.UnknownHostException; 
+
+/** Prototyp Klasse zum Ausprobieren 
+ * Chatroom mit Furkan und Jacques
+ * 
+ * @author Merdan
+ * 
+ * Diese Klasse ist für den Server, der die ChatroomGUI nutzt.
+ */
+public class ChatroomServer {
+	
+	static ServerSocket ss;
+	static Socket s;
+	static DataInputStream din;
+	static DataOutputStream dout;
+	
+	//private String ip = ChatroomLogin.getText(ip);
+	//private String port = ChatroomLogin.getText(port);
+	
+	public static void main(String[] args) {
+		
+		String msgin = "";
+		try {
+			ss= new ServerSocket(port);
+		}
+		catch(Exception e){
+			
+		}
+		
+	}
+}
