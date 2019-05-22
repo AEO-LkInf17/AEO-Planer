72e0786cc2cadd180086d4e24b2325822677fee5
edo <edo@192.168.0.15>
2019-05-22 12:43:14 +0200

    Kommentar für die Klasse Karte

--- eclipse/src/skipbo/logic/Karte.java
+++ eclipse/src/skipbo/logic/Karte.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
  * 
  * @author e_berardi
  * @author r_conforti
+ * 
+ * Die Klasse "Karte" bildet das Objekt "Karte" 
  */
 
 
 public class Karte 
 {
 
-	
+	// Attribute der Klasse Spieler 
 
 	private int zahl;
 	private boolean Skipbo;
@@ -24,41 +26,68 @@ public class Karte
 	
 	// public static final int SKIPBO=0, EINS=1, ZWEI=2, DREI=3, VIER=4, FÜNF=5, SECHS=6, SIEBEN=7, ACHT=8, NEUN=9, ZEHN=10, ELF=11, ZWÖLF=12;
 
+	/*
+	 * Konstruktor der Klasse Karte, verlangt zahl und Skipbo
+	 */
 	public Karte (int zahl, boolean Skipbo) {
 		this.zahl= zahl;
 		this.Skipbo= Skipbo;
 		
 	}
 	
-//
+	/*
+	 *get-Methode für den Wert (zahl) von die Karte 
+	 *@return zahl  
+	 */
 	public int getZahl ()
 	{
 		return zahl;
 	}
 	
+	/*
+	 * get-Methode für den Gewünschter Wert der Karte
+	 * @return gewueschterWert
+	 */
 	public int getGewuenschterWert() 
 	{
 		return this.gewuenschterWert;
 	}
 	
-	
+	/*
+	 * get-Methode ob Skipbo true or false ist
+	 * @return Skipbo
+	 */
  
 	public boolean getSkipbo() 
 	{
 		return Skipbo;
 	}
 	
+	/*
+	 * Set-Methode für zahl 
+	 */
+	
 	public void setZahl(int z) {
 		this.zahl = z;
 	}
 	
+	/*
+	 * Set-Methode für Gewünschter Wert 
+	 */
 	public void setGewuenschterWert(int gw) {
 		this.gewuenschterWert = gw;
 	}
 	
+	/*
+	 * Set-Methode für die Skipbo Karte 
+	 */
 	public void setSkipbo(boolean s) {
 		this.Skipbo = s;
 	}
+	
+	/*
+	 * toString Methode: wenn Skipbo true ist dann muss der Spieler eine Zahl wehlen  
+	 */
 	
 public String toString() {
 	  if(Skipbo == true) {
